dee4c9db1e59ee2196baa8caf7329f0bcd8200c1
Jun <Jun@Junui-MacBook-Pro.local>
2015-12-23 18:35:29 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('landing', {title: 'Now Panda'});
+});
+
+router.get('/order', function (req, res, next) {
+    res.render('item-view', {title: 'Now Panda'});
+});
+
+module.exports = router;
